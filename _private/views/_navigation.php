eebecbe25a084ea35333c1dad339004b24509ffa
Robin <24046@ma-web.nl>
2021-10-06 11:58:42 +0200

    VueJs CLI

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -11,12 +11,7 @@
     <li>
         <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
     </li>
-    <li>
-        <a href="<?php echo url( 'registren' ) ?>"<?php if ( current_route_is( 'registeren' ) ): ?> class="active"<?php endif ?>>Registeren</a>
-    </li>
 </ul>
-
-
 
 
 
