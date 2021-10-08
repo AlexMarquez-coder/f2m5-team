c3f03d2ea8ff376e1eeb218c22609d212e6746eb
Thies <Thiesvanderzon@gmail.com>
2021-10-08 13:02:29 +0200

    navbar gefixt

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,15 +1,15 @@
 <ul id="mainmenu">
     <li>
-        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Home</a>
     </li>
     <li>
-        <a href="<?php echo url( 'login' ) ?>"<?php if ( current_route_is( 'login' ) ): ?> class="active"<?php endif ?>>Login</a>
+        <a href="<?php echo url( 'login' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Login</a>
     </li>
     <li>
-        <a href="<?php echo url( 'over' ) ?>"<?php if ( current_route_is( 'over' ) ): ?> class="active"<?php endif ?>>Over ons</a>
+        <a href="<?php echo url( 'over' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Over ons</a>
     </li>
     <li>
-        <a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( 'contact' ) ): ?> class="active "<?php endif ?>>Contact</a>
+        <a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active "<?php endif ?>>Contact</a>
     </li>
     <li>
       <a class="menu-btn" href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Aanmelden</a>
