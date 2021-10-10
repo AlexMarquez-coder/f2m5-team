54d26815d095b56de70daccb713145d52bda4caf
Alex Marquez <marquezworld@gmail.com>
2021-10-10 10:29:59 +0200

    routes.php bestand aangewrkt

--- _private/routes.php
+++ _private/routes.php
@@ -14,7 +14,7 @@ SimpleRouter::group( [ 'prefix' => site_url() ], function () {
 	SimpleRouter::get( '/', 'WebsiteController@home' )->name( 'home' );
 	SimpleRouter::get( '/registreren', 'RegistrationController@registrationForm' )->name('register.form');
 	SimpleRouter::post( '/registreren/verwerken', 'RegistrationController@handleRegistrationForm' )->name('register.handle' );
-
+	SimpleRouter::get( '/registreren/bedankt', 'RegistrationController@registrationThankYou' )->name('register.ThankYou');
 
 
 	// STOP: Tot hier al je eigen URL's zetten, dit stukje laat de 4040 pagina zien als een route/url niet kan worden gevonden.
@@ -34,5 +34,4 @@ SimpleRouter::error( function ( Request $request, \Exception $exception ) {
 	}
 
 } );
-
 
