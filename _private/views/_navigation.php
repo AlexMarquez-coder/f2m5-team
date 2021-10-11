f51bf5aa2f771f637ddecb8be622a012fa5c514d
Robin <24046@ma-web.nl>
2021-10-11 10:35:27 +0200

    update nav

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,27 +1,7 @@
-<<<<<<< HEAD
+
 <nav id="mainmenu" class="flex items-center justify-between flex-wrap bg-grey-darkest p-6 fixed w-full z-10 pin-t">
 	<div class="flex items-center flex-no-shrink text-white mr-6">
 	</div>
-=======
-<ul id="mainmenu">
-    <li>
-        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Home</a>
-    </li>
-    <li>
-        <a href="<?php echo url( 'login' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Login</a>
-    </li>
-    <li>
-        <a href="<?php echo url( 'over' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Over ons</a>
-    </li>
-    <li>
-        <a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active "<?php endif ?>>Contact</a>
-    </li>
-    <li>
-      <button id="aanmeldknop"><a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Registreren</a></button> 
-    </li>
-</ul>
->>>>>>> 6dfb1015eeb363564ac4830407a678234ac7edf7
-
 	<div class="block lg:hidden">
 		<button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-white">
 			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
