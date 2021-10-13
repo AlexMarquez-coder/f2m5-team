b741a925e985af986913826e281f62334df4835c
Robin <24046@ma-web.nl>
2021-10-13 12:02:58 +0200

    back to old nav

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,38 +1,26 @@
 
-<ul id="mainmenu" class="flex items-center justify-between flex-wrap bg-grey-darkest p-6 fixed w-full z-10 pin-t">
-	<div class="flex items-center flex-no-shrink text-white mr-6">
-	</div>
-	<div class="block lg:hidden">
-		<button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-white hover:border-white">
-			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-		</button>
-	</div>
-
-	<div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block pt-6 lg:pt-0" id="nav-content">
-		<ul class="list-reset lg:flex justify-end flex-1 items-center">
-			<li class="mr-3">
-				<a class="inline-block py-2 px-4 text-white no-underline" href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?>class="inline-block py-2 px-4 text-white no-underline active"<?php endif ?>>Home</a>
-			</li>
-			<li class="mr-3">
-				<a  href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?> class="inline-block text-grey-dark no-underline hover:text-grey-lighter hover:text-underline py-2 px-4 active" <?php endif ?>>Register</a>
-			</li>
-			<li class="mr-3">
-				<a href="?php echo url( 'over' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="inline-block text-grey-dark no-underline hover:text-grey-lighter hover:text-underline py-2 px-4 active"  <?php endif ?>>Over ons</a>
-			</li>
-			<li class="mr-3">
-				<a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="inline-block text-grey-dark no-underline hover:text-grey-lighter hover:text-underline py-2 px-4 active" <?php endif ?>>Contact</a>
-			</li>
-			<li class="mr-3">
-				<a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="inline-block text-grey-dark no-underline hover:text-grey-lighter hover:text-underline py-2 px-4 active"  <?php endif ?>>aanmelden
-                </a>
-			</li>
-		</ul>
-	</div>
+<ul id="mainmenu">
+    <li>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
+    </li>
+    <li>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Login</a>
+    </li>
+    <li>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Over ons</a>
+    </li>
+    <li>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Contact</a>
+    </li>
+    <li>
+       <button id="aanmeldknop"><a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Aanmelden</a></button> 
+    </li>
+</ul>
+    <li>
+        <a href="<?php echo url( 'registren' ) ?>"<?php if ( current_route_is( 'registeren' ) ): ?> class="active"<?php endif ?>>Registeren</a>
+    </li>
 </ul>
 
-<script>
-	//Javascript to toggle the menu
-	document.getElementById('nav-toggle').onclick = function(){
-	document.getElementById("nav-content").classList.toggle("hidden");
-	}
-</script>
+
+
+
