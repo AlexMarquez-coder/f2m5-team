a0dac41d9122af6b8abd6e06b1a6ba109c6ab26e
Thies <Thiesvanderzon@gmail.com>
2021-10-15 12:00:58 +0200

    -

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,12 +1,6 @@
-<a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?> <?php endif ?>>
-<img src="/Logo simpel.webp" id="logo" > </img> </a>
 <ul id="mainmenu">
-
-	<div>
-	
-	</div>
     <li>
-        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?>  class="active"<?php endif ?>>Home</a>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Home</a>
     </li>
     <li>
         <a href="<?php echo url( 'over_ons' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Over ons</a>
