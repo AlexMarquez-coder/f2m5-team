7a2147bc1c2a63f5f487e68d76ec4cdae2215019
Thies <Thiesvanderzon@gmail.com>
2021-10-15 11:51:14 +0200

    nav gifixt

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -6,13 +6,13 @@
 	
 	</div>
     <li>
-        <a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?>  class="active"<?php endif ?>>Home</a>
+        <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?>  class="active"<?php endif ?>>Home</a>
     </li>
     <li>
         <a href="<?php echo url( 'over_ons' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Over ons</a>
     </li>
 	<li>
-        <a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Registeren</a>
+        <a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Contact</a>
     </li>
     <li>
        <button id="aanmeldknop"><a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Aanmelden</a></button> 
