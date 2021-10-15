b56803a47dd056845ae2446f29c50ba970204581
Alex Marquez <marquezworld@gmail.com>
2021-10-15 12:54:50 +0200

    Aan Automatische fotoslider gewerkt

--- _private/views/homepage.php
+++ _private/views/homepage.php
@@ -16,7 +16,7 @@
         Hier helpen en steunen we elkaar. <br>
         Ook worden er wekelijks praktische tips gedeeld die jou helpen om zelfverzekerd te zijn en om te gaan met moeilijke situaties.</p>
 
-<script>
+<!-- <script>
 let fotonummer, fotobreedte, aantalfotos, fotoslider;
         function setupFotoslider() 
         {
@@ -67,7 +67,60 @@ let fotonummer, fotobreedte, aantalfotos, fotoslider;
             <button id="volgende">></button>
         </div>
         </div>
-    </div>
+    </div> -->
+
+
+</head>
+<body>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <img src="/images/elwin.png" style="width:50%">
+</div>
+
+<div class="mySlides fade">
+  <img src="/images/Lisa.png" style="width:50%">
+</div>
+
+<div class="mySlides fade">
+  <img src="/images/yasmine.png" style="width:50%">
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+</body>
+</html> 
+
 
 
 <?php $this->start('footer')?>
