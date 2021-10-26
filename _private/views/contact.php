2947c9614335941002d00cf5e2760171ea476cd4
Alex Marquez <marquezworld@gmail.com>
2021-10-26 09:05:40 +0200

    Bezig met contacy page

--- _private/views/contact.php
+++ _private/views/contact.php
@@ -5,16 +5,27 @@
 
 <?php $this->stop()?>
 
-<h1>Welkom</h1>
 
-<p>Contact page</p>
+<div id="BGtext">
+    <div id="BGmissie">
+        <h1 id="missie"></h1>
+        <h3 id="missie2"></h3>
+    </div>
+        <div id="over_onsText">
+            <h2 id="over_onsTitel">
+        </div>
+</div>
 
+<h3>Hier komen de icons voor de oprichters.</h3>
 <?php $this->start('footer')?>
 
 <?php $this->stop()?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+
+
 
 
 
