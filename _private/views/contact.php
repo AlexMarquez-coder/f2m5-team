e9ea176ec2230d666ea88c008364ee77e3c4caa9
Alex Marquez <marquezworld@gmail.com>
2021-10-26 11:30:36 +0200

    Tussen commit voor contact pagina

--- _private/views/contact.php
+++ _private/views/contact.php
@@ -6,23 +6,54 @@
 <?php $this->stop()?>
 
 
-<div id="BGtext">
-    <div id="BGmissie">
-        <h1 id="missie"></h1>
-        <h3 id="missie2"></h3>
+<div id="BGcontactpagetext">
     </div>
-        <div id="over_onsText">
-            <h2 id="over_onsTitel">
+        <div id="contactText">
+            <h2 id="contactTitel">Contact en Informatie over ons</h2>
+            <br>           
+            <div id="kopje1"><strong>Dan ben je bij ons op het juiste adres! </strong></div>
+            Wij zijn een community van jongeren die allemaal met dit soort vragen worstelen. 
+            <br>
+            Binnen de Transformers Community delen we onze ervaringen, steunen elkaar én leren van mekaar.
+            <br>
+            De Transformers Community is een plek waar je écht jezelf mag zijn. 
+            <br>
+            Een plek waar je leert hoe je onzekerheden en tegenslagen omzet in kracht, 
+            <br>
+            en iets meer schijt hebt aan de mening van anderen ;-).
+            <br>
+            <br>
+            <div id="kopje2"><strong>Privacy</strong></div>
+            We zijn een besloten platform, 
+            wat betekent dat al jouw informatie binnen de community blijft 
+            <br>
+            en je data niet stiekem wordt doorverkocht. 
+            <br>
+            <br>
+            Note: Je kan je anoniem aanmelden door een andere naam te gebruiken dat je echte naam.
+            <br>
+            <br>
+        <div id="SM">
+            <a href="https://www.instagram.com/lifee.transformers/">
+                <img alt="Qries" src="/icons/instagram_new_icon_512px.png" height="50">
+
+            <a href="https://www.youtube.com/channel/UCvN0_IYGVy6s1jFj2enGIsw">
+                <img alt="Qries" src="/icons/youtube_icon_512px.png" height="50">
+                </div>
         </div>
 </div>
 
-<h3>Hier komen de icons voor de oprichters.</h3>
+
 <?php $this->start('footer')?>
 
 <?php $this->stop()?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+
+
+
 
 
 
