62c0bcd1e5673673197e129d4326c0d826bb0fec
Thies <Thiesvanderzon@gmail.com>
2021-11-02 12:39:10 +0100

    Oprichters erin gezet en grid daarvan responsive gemaakt

--- _private/views/over_ons.php
+++ _private/views/over_ons.php
@@ -27,8 +27,75 @@
         </div>
 </div><br>
 
-<h3>Hier komen de icons voor de oprichters.</h3>
-<img src="" alt="foto" class="oprichters"> 
+<h1 id="optext">Wie we  <span id="sometimes">zijn</span> </h1>
+
+<div class="oprichterContainer">
+    <div class="Iris">
+        <img src="../images/Iris.jpg"     alt="foto" class="oprichters">
+            <div class="naam">Iris</div>
+            <div class="functie">Oprichter</div>
+    </div>
+
+    <div class="Amzah">
+        <img src="../images/Amzah.jpg"     alt="foto" class="oprichters">
+            <div class="naam">Amzah</div>
+            <div class="functie">Bestuurslid</div>
+    </div>
+
+    <div class="Annebregt">
+        <img src="../images/Annebregt.jpg"alt="foto" class="oprichters"> 
+            <div class="naam">Annebregt</div>
+            <div class="functie">Bestuurslid</div>
+    </div>
+
+    <div class="Mariam">
+        <img src="../images/Mariam.jpg"   alt="foto" class="oprichters">
+            <div class="naam">Mariam</div>
+            <div class="functie">Bestuurslid</div>
+    </div>
+
+    <div class="Elwin">
+        <img src="../images/Elwin.jpg"    alt="foto" class="oprichters">
+            <div class="naam">Elwin</div>
+            <div class="functie">Ambassadeur</div> 
+    </div>
+
+    <div class="Andrea">
+        <img src="../images/Andrea.jpg"   alt="foto" class="oprichters"> 
+            <div class="naam">Andrea</div>
+            <div class="functie">Ambassadeur en Social media</div> 
+    </div>
+
+    <div class="Sanne">
+        <img src="../images/Sanne.jpg"    alt="foto" class="oprichters">
+            <div class="naam">Sanne</div>
+            <div class="functie">Ambassadeur en Audioviseel</div>  
+    </div>
+
+    <div class="Estrella">
+        <img src="../images/Estrella.jpg" alt="foto" class="oprichters"> 
+            <div class="naam">Estrella</div>
+             <div class="functie">Ambassadeur en Coach</div> 
+    </div>
+
+    <div class="Sacha">
+        <img src="../images/Sacha.jpg"    alt="foto" class="oprichters"> 
+            <div class="naam">Sacha</div>
+            <div class="functie">Ambassadeur en Schrijfster</div> 
+    </div>
+
+    <div class="Yulia">
+        <img src="../images/Yulia.jpg"    alt="foto" class="oprichters"> 
+            <div class="naam">Yulia</div>
+            <div class="functie">Grafische vormgeving Brain Boost Box</div> 
+    </div>
+
+    <div class="Evert">
+        <img src="../images/Evert.jpg"    alt="foto" class="oprichters"> 
+            <div class="naam">Evert</div>
+            <div class="functie">Audioviseel</div>
+    </div>
+</div>
 
 <?php $this->start('footer')?>
 
