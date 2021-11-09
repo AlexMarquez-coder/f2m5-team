38b50afb907363a3e5832eec4aae3453fb2a0904
Robin <24046@ma-web.nl>
2021-11-09 11:58:15 +0100

    footer final push

--- _private/views/footer.php
+++ _private/views/footer.php
@@ -1,10 +1,46 @@
-	<div id="footer">
-        <div>
+	<div id="footer" class="item" style="margin-top: 3%;">
+        <div style="margin-left: 2%;" class="footer_item1">
         	<ul>
                 <li><a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Home</a>	</li>
                 <li><a href="<?php echo url( 'over_ons' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Over ons</a>	</li>
                 <li><a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Contact</a></li>		
-                <li><a href="<?php echo url( 'blog.detail' ) ?>"<?php if ( current_route_is( 'index' ) ): ?> class="active"<?php endif ?>>Tijdelijk Blog</a></li>
-            </ul>
+                <li> <a href="<?php echo url( 'blog.detail' ) ?>"<?php if ( current_route_is( 'index' ) ): ?> class="active"<?php endif ?>>Tijdelijk Blog</a></li>
+             </ul>	
         </div>
+         <div class="footer_item2">
+            
+                <p><b>Volg ons op social media:</b></p>
+             <ul>
+             <li><a href="https://www.instagram.com/transformer.community/">Instagram</a></li>
+             <li><a href="https://www.linkedin.com/company/transformerscommunity/">LinkedIN</a></li>
+             <li><a href="https://www.facebook.com/transformers.community">Facebook</a></li>
+             <li><a href="https://www.youtube.com/channel/UCvN0_IYGVy6s1jFj2enGIsw">Youtube</a></li>
+             </ul>
+         </div>
+        <div class="footer_item3">
+        <h4><b>Transformers Community</b></h4>
+        <p><b>Wij empoweren jongeren om mentaal gezond te zijn.</b></p>
+        <p>Stichting Transformers Community is geregistreerd bij de Kamer van Koophandel onder RSIN nummer 861477194.</p>
+
+        </div>
+        
     </div>
+ 
+
+ <style>
+.footer_item1{
+
+padding-left: 3%;
+}
+
+.footer_item2{
+    padding-left: 25%;
+}    
+
+.footer_item3{
+    padding-left: 25%;
+    text-align: left;
+
+}   
+ </style>
+
