a3be96756757ed0008ed01ea5e12cd156b62902a
Thies <Thiesvanderzon@gmail.com>
2021-11-09 10:55:02 +0100

    footer fix2.0

--- _private/views/homepage.php
+++ _private/views/homepage.php
@@ -101,28 +101,13 @@
           Wil jij je ook inzetten voor een samenleving waarin onze mentale gezondheid centraal staat?<br>
           Meld je dan aan als vrijwilliger!</p>
       </div>
-      <div>
-      	<div id="footer">
-        <div>
-        	<ul>
-                <li><a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Home</a>	</li>
-                <li><a href="<?php echo url( 'over_ons' ) ?>"<?php if ( current_route_is( '' ) ): ?>  class="active"<?php endif ?>>Over ons</a>	</li>
-                <li><a href="<?php echo url( 'contact' ) ?>"<?php if ( current_route_is( '' ) ): ?> class="active"<?php endif ?>>Contact</a></li>		
-                <li><a href="<?php echo url( 'blog.detail' ) ?>"<?php if ( current_route_is( 'index' ) ): ?> class="active"<?php endif ?>>Tijdelijk Blog</a></li>
-            </ul>
-        </div>
-      </div>
-
-
       </div>
 
     </div>
   </div>
 
 
-<?php $this->start('footer') ?>
 
-<?php $this->stop() ?>
 
   <script>
     var slideIndex = 0;
